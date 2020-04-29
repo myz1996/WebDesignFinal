d169f7b1ec79d65f65a88b2f1cb899c1cfc2ae1c
709484909 <709484909@qq.com>
2020-04-29 19:19:09 -0400

    final team work

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,15 +1,15 @@
-const User = require('../models/user')
-const {errorHandler} = require("../helpers/dbErrorHandler");
-const jwt = require('jsonwebtoken');// to generate signed token
-const expressJwt = require('express-jwt'); // for authrozation check
+const User = require("../models/user");
+const jwt = require("jsonwebtoken"); // to generate signed token
+const expressJwt = require("express-jwt"); // for authorization check
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signup = (req, res) => {
-    console.log("req.body", req.body);
+    // console.log("req.body", req.body);
     const user = new User(req.body);
     user.save((err, user) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
-                err: errorHandler(err)
+                error: errorHandler(err)
             });
         }
         user.salt = undefined;
@@ -20,65 +20,56 @@ exports.signup = (req, res) => {
     });
 };
 
-
-
-
-
 exports.signin = (req, res) => {
-    //find the user based on email
-    const {email, password} = req.body
-    User.findOne({email}, (err, user) => {
-        if(err || !user){
+    // find the user based on email
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
             return res.status(400).json({
-                error: 'User with that email does not exist.'
+                error: "User with that email does not exist. Please signup"
             });
         }
-        // if user is found make sure email and password match
-        //create authenticate method in user model
-
-        if(!user.authenticate(password)) {
+        // if user is found make sure the email and password match
+        // create authenticate method in user model
+        if (!user.authenticate(password)) {
             return res.status(401).json({
-                error: 'Email and password do not match!'
-            })
+                error: "Email and password dont match"
+            });
         }
-        //generate a signed token with user id and secret
-        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
-        //persist the token as 't' in cookie with expiry date
-        res.cookie('t', token, {expire: new Date() + 9999})
-        //return response with user and token ro frontend user
-        const {_id, name, email, role} = user;
-        return res.json({token, user: {_id, email, name, role} });
+        // generate a signed token with user id and secret
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        // persist the token as 't' in cookie with expiry date
+        res.cookie("t", token, { expire: new Date() + 9999 });
+        // return response with user and token to frontend client
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, email, name, role } });
     });
 };
 
-
-
 exports.signout = (req, res) => {
-    res.clearCookie('t');
-    res.json({message: 'Signout successfuly'});
+    res.clearCookie("t");
+    res.json({ message: "Signout success" });
 };
-
 
 exports.requireSignin = expressJwt({
     secret: process.env.JWT_SECRET,
     userProperty: "auth"
 });
 
-
 exports.isAuth = (req, res, next) => {
-    let user = req.profile && req.auth && req.profile._id == req.auth._id
-        if(!user) {
-            return res.status(403).json({
-                error: "Access denied"
-            });
-        }
-        next();
+    let user = req.profile && req.auth && req.profile._id == req.auth._id;
+    if (!user) {
+        return res.status(403).json({
+            error: "Access denied"
+        });
+    }
+    next();
 };
 
 exports.isAdmin = (req, res, next) => {
-    if(req.profile.role === 0) {
+    if (req.profile.role === 0) {
         return res.status(403).json({
-            error: "admin resource! Acess denied!"
+            error: "Admin resourse! Access denied"
         });
     }
     next();
--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,11 +1,11 @@
-const Category  = require("../models/category");
-const {errorHandler} = require("../helpers/dbErrorHandler");
+const Category = require("../models/category");
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.categoryById = (req, res, next, id) => {
     Category.findById(id).exec((err, category) => {
         if (err || !category) {
             return res.status(400).json({
-                error: 'Category does not exist'
+                error: "Category does not exist"
             });
         }
         req.category = category;
@@ -14,14 +14,14 @@ exports.categoryById = (req, res, next, id) => {
 };
 
 exports.create = (req, res) => {
-    const category = new Category(req.body)
+    const category = new Category(req.body);
     category.save((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             });
         }
-        res.json({data});
+        res.json({ data });
     });
 };
 
@@ -30,35 +30,35 @@ exports.read = (req, res) => {
 };
 
 exports.update = (req, res) => {
-    const category = req.category
-    category.name = req.body.name
+    const category = req.category;
+    category.name = req.body.name;
     category.save((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
-            })
+            });
         }
         res.json(data);
     });
 };
 
 exports.remove = (req, res) => {
-    const category = req.category
+    const category = req.category;
     category.remove((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
-            })
+            });
         }
         res.json({
-            message: 'Category deleted'
+            message: "Category deleted"
         });
     });
 };
 
 exports.list = (req, res) => {
     Category.find().exec((err, data) => {
-        if(err) {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             });
